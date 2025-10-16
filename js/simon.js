6bbeddb6a81499dd734642be9f1c67857116ee95
ousemaakacha <akachaousema@gmail.com>
2025-10-16 15:02:38 +0200

    diplayed the random numb with text

--- js/simon.js
+++ js/simon.js
@@ -6,7 +6,12 @@ const randomNumb = [];
 const guessNumb = 5;
 const maxNumb = 100;
 
-for (let i = 0; i, guessNumb; i++) {
+for (let i = 0; i < guessNumb; i++) {
   let n = Math.floor(Math.random() * maxNumb) + 1;
   randomNumb.push(n);
 }
+
+// prendo la classe che mi interessa dalla dom e stampo un messago per memorizzare
+const numbDiv = document.getElementById("numeri");
+
+numbDiv.innerHTML = `MEMORIZZA QUESTI NUMERI ${randomNumb.join()}`;

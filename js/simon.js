72604d6257dd109b1250d206184fe01b1c3aa167
ousemaakacha <akachaousema@gmail.com>
2025-10-16 14:47:10 +0200

    loop for random  number completed

--- js/simon.js
+++ js/simon.js
@@ -5,3 +5,8 @@ console.log("SIMON");
 const randomNumb = [];
 const guessNumb = 5;
 const maxNumb = 100;
+
+for (let i = 0; i, guessNumb; i++) {
+  let n = Math.floor(Math.random() * maxNumb) + 1;
+  randomNumb.push(n);
+}
